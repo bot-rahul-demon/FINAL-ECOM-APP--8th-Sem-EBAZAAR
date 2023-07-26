22c801622a0d75f608022293afd3cae27b937fbe
bot-rahul-demon <132208094+bot-rahul-demon@users.noreply.github.com>
2023-07-26 14:55:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //configure env
 dotenv.config();
 
